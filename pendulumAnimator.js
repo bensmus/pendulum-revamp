a7ccab8bdd1e898b3bef1d56748a84c54cbfa617
Ben Smus <bensmus@gmail.com>
2022-01-06 11:08:29 -0800

    idk

--- pendulumAnimator.js
+++ pendulumAnimator.js
@@ -11,20 +11,4 @@ setInterval(function drawstep() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     pendulum.draw();
     pendulum.step(0.01); // will decreasing step size reduce errors? yes.
-
-    //// Error metric /////
-    if (pendulum.state.angle + Math.PI < 0) {
-        violatingFlag = true;
-    }
-    if (violatingFlag && pendulum.state.angle + Math.PI >= 0) {
-        console.log(Math.min(...negativeAngles));
-        violatingFlag = false;
-        negativeAngles = [];
-    }
-    if (violatingFlag) {
-        negativeAngles.push(pendulum.state.angle + Math.PI);
-    }
-    //////////////////////
-    // error is 0.05028986679342662 for 0.01 step
-    // error is 0.269739574312311 for 0.05 step
 }, 10); // 10 ms -> approx 60 hz

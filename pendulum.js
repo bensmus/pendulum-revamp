df271416c0cf0d0152c5f5717bd7dfdc7f3ec2b6
Ben Smus <bensmus@gmail.com>
2022-01-08 13:07:18 -0800

    trail not working

--- pendulum.js
+++ pendulum.js
@@ -6,6 +6,22 @@ let canvasDim = 500;
 document.getElementById("canvas").setAttribute("width", canvasDim.toString());
 document.getElementById("canvas").setAttribute("height", canvasDim.toString());
 
+///////// FROM STACKOVERFLOW ////////
+let canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+function drawPixel(x, y, r, g, b, a) {
+    let index = (x + y * canvas.width) * 4;
+    canvasData.data[index + 0] = r;
+    canvasData.data[index + 1] = g;
+    canvasData.data[index + 2] = b;
+    canvasData.data[index + 3] = a;
+}
+
+function updateCanvas() {
+    ctx.putImageData(canvasData, 0, 0);
+}
+/////////////////////////////////////
+
 // Pendulum composed of links.
 // e.g: link = { ang: 3, vel: 2, r: 10 };
 
@@ -30,10 +46,45 @@ function drawLink(link, linkOrigin) {
 
 function singlePendulumUpdate(link, delta) {
     let angAccel = -Math.cos(link.ang);
-    link.vel += angAccel * delta;
-    link.ang += link.vel * delta;
+    linkUpdate(link, angAccel, delta);
 }
 
 function doublePendulumUpdate(link1, link2, delta) {
     // Double pendulum with mass at the end.
+    // The larger numbered link is closer to the mass.
+    let angAccel2 = -Math.cos(link2.ang);
+    let accel1 = link2.r * angAccel2 * Math.cos(link1.ang - link2.ang);
+    let angAccel1 = accel1 / link1.r;
+    linkUpdate(link2, angAccel2, delta);
+    linkUpdate(link1, angAccel1, delta);
 }
+
+function linkUpdate(link, angAccel, delta) {
+    link.vel += angAccel * delta;
+    link.ang += link.vel * delta;
+}
+
+function doublePendulumAnimate(linkOrigin, link1, link2) {
+    let endPoints = [];
+    setInterval(function drawstep() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw the past trail
+        endPoints.forEach((point) => {
+            drawPixel(point.x, point.y, 0, 0, 0, 1);
+        });
+        updateCanvas();
+
+        // Draw the pendulum
+        let linkPoint = linkOrigin;
+        linkPoint = drawLink(link1, linkPoint);
+        let endPoint = drawLink(link2, linkPoint);
+        endPoints.push(endPoint);
+        doublePendulumUpdate(link1, link2, 0.01); // will decreasing step size reduce errors? yes.
+    }, 10); // 10 ms -> approx 60 hz
+}
+
+link1 = { ang: 0, vel: 0, r: 1 };
+link2 = { ang: 0, vel: 1, r: 1 };
+linkOrigin = { x: 200, y: 200 };
+doublePendulumAnimate(linkOrigin, link1, link2);
